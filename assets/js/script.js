e2c2cdc96c9ab618d01852e446a96b83a10b7434
Bohdan Bezushka Perepelytsia <bbezushka@gmail.com>
2022-11-08 23:54:44 +0000

    Add sandWatch function

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,9 @@ let movements = 0;
 let moves= document.getElementById("moves");
 let identities = 0;
 let identified= document.getElementById("identified");
+let watch = false;
+let time = document.getElementById("time");
+let timer = 60;
 
 //Creation of an array that will be used to appear on the buttons.
 let numbers = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8]
@@ -18,11 +21,18 @@ console.log(numbers);
 
 //This is the function that allows the button to be turned around so that the player can see the content.
 function popUp(id){
+
+    //Starts the countdown when the user opens the first button.
+    if (watch === false){
+        watch = true;
+        sandWatch();
+    }
+
     //Displays the number of times a button has been uncovered.
     uncoveredBottoms++;
     console.log(uncoveredBottoms);
 
-    if(uncoveredBottoms == 1){
+    if(uncoveredBottoms === 1){
         buttonOne = document.getElementById(id);
         firsResult = numbers[id]
         buttonOne.innerHTML= firsResult;
@@ -30,7 +40,7 @@ function popUp(id){
         //Lock the first button (the player can see it) to unlock the second button.
         buttonOne.disabled = true;
 
-    }else if(uncoveredBottoms == 2){
+    }else if(uncoveredBottoms === 2){
         buttonTwo = document.getElementById(id);
         secondResult = numbers[id];
         buttonTwo.innerHTML = secondResult;
@@ -47,7 +57,7 @@ function popUp(id){
         
         /*This "if" works to compare the first result and the second result to see if they are the same.If the buttons
         are the same, the variable "uncoveredBottoms" returns to the value 0.*/
-        if(firsResult == secondResult){
+        if(firsResult === secondResult){
             uncoveredBottoms= 0;
 
             
@@ -56,12 +66,12 @@ function popUp(id){
             identified.innerHTML = `Identified: ${identities}`;
 
 
-
-            if(identities == 8 && movements == 8){
+            //Here we notify the player that he/she has completed the challenge if he/she reaches 8 matches.
+            if(identities === 8 && movements == 8){
                 moves.innerHTML = `PERFECT!!!`;
                 identified.innerHTML = 'CHALLENGE COMPLETED!!!';
 
-            }if(identities == 8 && movements > 8){
+            }if(identities === 8 && movements > 8){
                 moves.innerHTML = `You could do better.`;
                 identified.innerHTML = 'Challenge completed.';
             }
@@ -76,11 +86,14 @@ function popUp(id){
 
                 uncoveredBottoms = 0;
             },0500);
-
         }
-    
-
     }
+}
 
-    
+//This is the function that makes the time start to count down.
+function sandWatch(){
+    setInterval(() => {
+        timer --;
+        time.innerHTML = `Time: ${timer} sec.`;
+    },1000)
 }
